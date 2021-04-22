6a179dc545daf971a94d78b0e7b5f56035176b03
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2021-04-22 13:21:30 -0400

    Add project files.

---
+++ Minesweeper2021SP/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Minesweeper2021SP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            // this.button1 = new System.Windows.Forms.Button();
+            // this.button2 = new System.Windows.Forms.Button();
+            // this.button3 = new System.Windows.Forms.Button();
+            // this.button4 = new System.Windows.Forms.Button();
+            // this.button5 = new System.Windows.Forms.Button();
+            // this.button6 = new System.Windows.Forms.Button();
+            // this.tableLayoutPanel1.SuspendLayout();
+            // this.SuspendLayout();
+            // // 
+            // // tableLayoutPanel1
+            // // 
+            // this.tableLayoutPanel1.ColumnCount = 3;
+            // this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.3F));
+            // this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.3F));
+            // this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.3F));
+            // this.tableLayoutPanel1.Controls.Add(this.button6, 2, 1);
+            // this.tableLayoutPanel1.Controls.Add(this.button5, 0, 1);
+            // this.tableLayoutPanel1.Controls.Add(this.button4, 2, 0);
+            // this.tableLayoutPanel1.Controls.Add(this.button3, 1, 0);
+            // this.tableLayoutPanel1.Controls.Add(this.button2, 0, 0);
+            // this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            // this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            // this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            // this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // this.tableLayoutPanel1.RowCount = 2;
+            // this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            // this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            // this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 703);
+            // this.tableLayoutPanel1.TabIndex = 0;
+            // // 
+            // // button1
+            // // 
+            // this.button1.Location = new System.Drawing.Point(269, 354);
+            // this.button1.Name = "button1";
+            // this.button1.Size = new System.Drawing.Size(150, 46);
+            // this.button1.TabIndex = 5;
+            // this.button1.Text = "button1";
+            // this.button1.UseVisualStyleBackColor = true;
+            // // 
+            // // button2
+            // // 
+            // this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            // this.button2.Location = new System.Drawing.Point(3, 3);
+            // this.button2.Name = "button2";
+            // this.button2.Size = new System.Drawing.Size(260, 345);
+            // this.button2.TabIndex = 4;
+            // this.button2.Text = "button2";
+            // this.button2.UseVisualStyleBackColor = true;
+            // // 
+            // // button3
+            // // 
+            // this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            // this.button3.Location = new System.Drawing.Point(269, 3);
+            // this.button3.Name = "button3";
+            // this.button3.Size = new System.Drawing.Size(260, 345);
+            // this.button3.TabIndex = 0;
+            // this.button3.Text = "button3";
+            // this.button3.UseVisualStyleBackColor = true;
+            // // 
+            // // button4
+            // // 
+            // this.button4.Location = new System.Drawing.Point(535, 3);
+            // this.button4.Name = "button4";
+            // this.button4.Size = new System.Drawing.Size(150, 46);
+            // this.button4.TabIndex = 1;
+            // this.button4.Text = "button4";
+            // this.button4.UseVisualStyleBackColor = true;
+            // // 
+            // // button5
+            // // 
+            // this.button5.Location = new System.Drawing.Point(3, 354);
+            // this.button5.Name = "button5";
+            // this.button5.Size = new System.Drawing.Size(150, 46);
+            // this.button5.TabIndex = 2;
+            // this.button5.Text = "button5";
+            // this.button5.UseVisualStyleBackColor = true;
+            // // 
+            // // button6
+            // // 
+            // this.button6.Location = new System.Drawing.Point(535, 354);
+            // this.button6.Name = "button6";
+            // this.button6.Size = new System.Drawing.Size(150, 46);
+            // this.button6.TabIndex = 3;
+            // this.button6.Text = "button6";
+            // this.button6.UseVisualStyleBackColor = true;
+            // // 
+            // // Form1
+            // // 
+            // this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            // this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            // this.ClientSize = new System.Drawing.Size(800, 703);
+            // this.Controls.Add(this.tableLayoutPanel1);
+            // this.Name = "Form1";
+            // this.Text = "Form1";
+            // this.tableLayoutPanel1.ResumeLayout(false);
+            // this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+
+    }
+}
+
---
+++ Minesweeper2021SP/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper2021SP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Rows = 10;
+            Columns = 4;
+            //InitializeComponent();
+            InitializeComponent2();
+        }
+
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        private System.Windows.Forms.Button[,] _buttons;
+        private void InitializeComponent2()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            _buttons = new Button[Rows, Columns];
+            for (int r = 0; r < Rows; r++)
+            for (int c = 0; c < Columns; c++)
+            {
+                _buttons[r, c] = new Button {Dock = DockStyle.Fill, TabIndex = r * Columns + c};
+            }
+                    
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = Columns;
+            for (int c = 0; c < Columns; c++)
+                this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F / Columns));
+            for (int r = 0; r < Rows; r++)
+                this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F / Rows));
+
+            for (int r = 0; r < Rows; r++)
+            for (int c = 0; c < Columns; c++)
+                    this.tableLayoutPanel1.Controls.Add(_buttons[r,c], c, r);
+
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = Rows;
+
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 703);
+            this.tableLayoutPanel1.TabIndex = 0;
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 703);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+    }
+}
